ba222f701c762878c1112600e6d825dceb61cb21
Kasymkhan <55043545+Kasymkhankhubiyev@users.noreply.github.com>
2022-10-07 10:12:30 +0700

    Add files via upload

--- ForestTown.py
+++ ForestTown.py
@@ -50,18 +50,25 @@ x = pos.x - east_wall_length-7
 buildCastle(x, pos.y, pos.z+6+north_wall_length, tower_height+2, tower_width+2, tower_length+2, 1)
 
 #backwall
-length = west_wall_length+east_wall_length+towersdist+ 2* tower_length
-scwall.WestPropWall(x+7, pos.y, pos.z+(tower_length + 2)+north_wall_length+1, length , tower_height - 2, tower_width - 2)
+backlength = west_wall_length+east_wall_length+towersdist+ 2* tower_length
+scwall.WestPropWall(x+7, pos.y, pos.z+(tower_length + 2)+north_wall_length+1, backlength , tower_height - 2, tower_width - 2)
 
 #расставляем башни по периметру
 step = 20
+
+#front
 for i in range(step, east_wall_length, step): #вправо -x
     if (east_wall_length - (i + 5*(i/step))) >= step:
-        buildCastle(pos.x-i-5*(i/step), pos.y, pos.z-1, tower_height, tower_width, tower_length, 0)
+        buildCastle(pos.x-i-5*(i/step), pos.y, pos.z, tower_height, tower_width, tower_length, 0)
 
 for i in range(step, west_wall_length, step): #влево +x
-    if (east_wall_length - (i + 5*(i/step))) >= step:
-        buildCastle(pos.x+2*tower_length+towersdist+i, pos.y, pos.z-1, tower_height, tower_width, tower_length, 0)
+    if (west_wall_length - (i + 5*(i/step))) >= step:
+        buildCastle(pos.x+2*tower_length+towersdist+i, pos.y, pos.z, tower_height, tower_width, tower_length, 0)
+        
+#backside
+for i in range(step, backlength, step): #влево +x
+    if (backlength - (i + 5*(i//step))) >= step:
+        buildCastle(pos.x-east_wall_length+i+5*(i//step), pos.y, pos.z+7+north_wall_length, tower_height, tower_width, tower_length, 1)
 
 
 
